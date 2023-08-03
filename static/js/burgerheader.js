6ae0603cf364ec29c4d416b5f5255e0f38e3df95
Evgeniy Tikhonenkov <leoevgeniy@gmail.com>
2023-08-03 12:10:26 +0400

    offcanvas serachbox autoanimation

---
+++ static/js/burgerheader.js
@@ -0,0 +1,14 @@
+
+
+const offcanvas_menu = document.getElementById('offcanvasRight')
+const burger = document.getElementById('burgermenu')
+
+offcanvas_menu.addEventListener('shown.bs.offcanvas',  (e) => {
+    const searchinput = document.getElementById('search-input')
+    const searchbutton = document.getElementById('offcanvassearchbutton')
+    searchbutton.click()
+    searchinput.focus()
+    searchbutton.click()
+
+    }
+)
