6d840f5944873a26f9bb3686493e0bad792e5f12
Evgeniy Tikhonenkov <leoevgeniy@gmail.com>
2023-08-08 14:58:47 +0400

    cart icon animation

--- static/js/cart.js
+++ static/js/cart.js
@@ -2,7 +2,7 @@ window.addEventListener( "pageshow", function ( event ) {
     let historyTraversal = event.persisted || ( typeof window.performance != "undefined" && window.performance.navigation.type === 2 );
     if ( historyTraversal ) {
         // Handle page restore.
-        window.location.reload();
+        window.location.replace(location.pathname);
     }
 });
 let iOS = navigator.userAgent.match(/iPhone|iPad|iPod|Mac OS/i);
